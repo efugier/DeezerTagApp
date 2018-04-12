748a825886a792f4e6b92e0ffdc03c6e6b2e0079
EFugier <emilienfugier@gmail.com>
2018-04-12 14:19:05 +0200

    templated search function

--- tag-front/src/App.vue
+++ tag-front/src/App.vue
@@ -16,8 +16,8 @@
             <b-navbar-nav class="ml-auto">
 
               <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="coma-separated tags"/>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                <b-form-input size="sm" class="mr-sm-2" type="text" v-model="tags" placeholder="coma-separated tags"/>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit" @click="search">Search</b-button>
               </b-nav-form>
 
             </b-navbar-nav>
@@ -68,11 +68,18 @@ export default {
   data () {
     return {
       label: 'track',
+      tags: '',
       options: [
         { text: 'Track', value: 'track' },
         { text: 'Album', value: 'album' },
         { text: 'Artist', value: 'artist' }
       ]
+    }
+  },
+
+  methods: {
+    async search () {
+      console.log(this.tags.split(/, */))
     }
   },
 
