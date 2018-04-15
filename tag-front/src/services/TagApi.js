414bc72049b6a13338ea159a6ee94990b39191bc
EFugier <emilienfugier@gmail.com>
2018-04-16 00:55:54 +0200

    fixed docker

--- tag-front/src/services/TagApi.js
+++ tag-front/src/services/TagApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    // baseURL: `http://localhost:8081`
-    baseURL: `http://tag-server`
+    baseURL: `http://localhost:8081`
+    // baseURL: `http://tag-server`
   })
 }
